1004d72e4f85f366484f6795b7eec53f4bf3cebc
KonradZ78 <148917868+KonradZ78@users.noreply.github.com>
2023-10-30 00:40:19 +0100

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
+
 class Informer {
 public:
     Informer() {
-        std::cout << "Informer-konstrukcja" << std::endl;
+        std::cout << "Informer constructed." << std::endl;
     }
 
     ~Informer() {
-        std::cout << "Informer-destrukcja" << std::endl;
+        std::cout << "Informer destructed." << std::endl;
     }
 };
 
@@ -21,12 +22,12 @@ private:
 public:
     Wektor2D(double x_val = 0.0, double y_val = 0.0) : x(x_val), y(y_val) {
         num_wek++;
-        std::cout << "Wektor (" << x << ", " << y << ") stworzony. Ilosc wektorow: " << num_wek << std::endl;
+        std::cout << "Wektor (" << x << ", " << y << ") constructed. Total vectors: " << num_wek << std::endl;
     }
 
     ~Wektor2D() {
         num_wek--;
-        std::cout << "Wektor (" << x << ", " << y << ") zniszczony. Ilosc wektorow " << num_wek << std::endl;
+        std::cout << "Wektor (" << x << ", " << y << ") destructed. Remaining vectors: " << num_wek << std::endl;
     }
 
     static Wektor2D kart(double x_val, double y_val) {
@@ -67,12 +68,12 @@ public:
         return num_wek;
     }
 
-    friend Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2) {
-        return Wektor2D(v1.x + v2.x, v1.y + v2.y);
+    Wektor2D operator*(double scalar) const {
+        return Wektor2D(x * scalar, y * scalar);
     }
 
-    friend Wektor2D operator*(const Wektor2D& v, double scalar) {
-        return Wektor2D(v.x * scalar, v.y * scalar);
+    friend Wektor2D operator+(const Wektor2D& v1, const Wektor2D& v2) {
+        return Wektor2D(v1.x + v2.x, v1.y + v2.y);
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Wektor2D& vec) {
@@ -88,22 +89,22 @@ int main() {
         Wektor2D v1 = Wektor2D::kart(3.0, 4.0);
         Wektor2D v2 = Wektor2D::bieg(5.0, 3.14 / 3);
 
-        std::cout << "Wektor 1: ";
+        std::cout << "Vector 1: ";
         v1.print();
-        std::cout << ", Norma: " << v1.norm() << std::endl;
+        std::cout << ", Norm: " << v1.norm() << std::endl;
 
-        std::cout << "Wektor 2: ";
+        std::cout << "Vector 2: ";
         v2.print();
-        std::cout << ", Norma: " << v2.norm() << std::endl;
+        std::cout << ", Norm: " << v2.norm() << std::endl;
 
         Wektor2D v3 = v1 + v2;
-        std::cout << "Wektor 3 (v1 + v2): " << v3 << std::endl;
+        std::cout << "Vector 3 (v1 + v2): " << v3 << std::endl;
 
         Wektor2D v4 = v2 * 2.0;
-        std::cout << "Wektor 4 (v2 * 2): " << v4 << std::endl;
+        std::cout << "Vector 4 (v2 * 2): " << v4 << std::endl;
     }
 
-    std::cout << "Pozostalo wektorow: " << Wektor2D::populacja() << std::endl;
+    std::cout << "Remaining vectors: " << Wektor2D::populacja() << std::endl;
 
     return 0;
 }
