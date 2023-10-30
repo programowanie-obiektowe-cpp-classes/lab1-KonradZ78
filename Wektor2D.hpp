4e6e716832b31eb0f96c81a059a604e7cd1e8f43
KonradZ78 <148917868+KonradZ78@users.noreply.github.com>
2023-10-30 01:27:23 +0100

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 
 class Informer {
 public:
@@ -20,7 +20,12 @@ private:
     Informer informer;
 
 public:
-    Wektor2D(double x_val = 0.0, double y_val = 0.0) : x(x_val), y(y_val) {
+    Wektor2D() : x(0.0), y(0.0) {
+        num_wek++;
+        std::cout << "Wektor (" << x << ", " << y << ") constructed. Total vectors: " << num_wek << std::endl;
+    }
+
+    Wektor2D(double x_val, double y_val) : x(x_val), y(y_val) {
         num_wek++;
         std::cout << "Wektor (" << x << ", " << y << ") constructed. Total vectors: " << num_wek << std::endl;
     }
@@ -68,12 +73,12 @@ public:
         return num_wek;
     }
 
-    Wektor2D operator*(double scalar) const {
-        return Wektor2D(x * scalar, y * scalar);
-    }
-
     Wektor2D operator+(const Wektor2D& other) const {
         return Wektor2D(x + other.x, y + other.y);
+    }
+
+    double operator*(const Wektor2D& other) const {
+        return x * other.x + y * other.y;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Wektor2D& vec) {
@@ -84,26 +89,23 @@ public:
 
 int Wektor2D::num_wek = 0;
 
-int main() 
-    {
-        Wektor2D v1 = Wektor2D::kart(3.0, 4.0);
-        Wektor2D v2 = Wektor2D::bieg(5.0, 3.14 / 3);
+int main() {
+    Wektor2D v1{}; // Konstruktor domyślny, wektor o wsp. [0, 0]
+    v1.setX(1.);   // setter
+    v1.setY(1.);   // setter
+    double x1 = v1.getX(); // getter
+    double y1 = v1.getY(); // getter
 
-        std::cout << "Vector 1: ";
-        v1.print();
-        std::cout << ", Norm: " << v1.norm() << std::endl;
+    Wektor2D v2 = Wektor2D::kart(2., 2.); // Użycie metody statycznej kart
 
-        std::cout << "Vector 2: ";
-        v2.print();
-        std::cout << ", Norm: " << v2.norm() << std::endl;
+    Wektor2D sum = v1 + v2; // dodawanie wektorów
 
-        Wektor2D v3 = v1 + v2;
-        std::cout << "Vector 3 (v1 + v2): " << v3 << std::endl;
+    double prod = v1 * v2; // iloczyn skalarny
 
-        Wektor2D v4 = v2 * 2.0;
-        std::cout << "Vector 4 (v2 * 2): " << v4 << std::endl;
-    
-
+    std::cout << "v1: " << v1 << ", x1: " << x1 << ", y1: " << y1 << std::endl;
+    std::cout << "v2: " << v2 << std::endl;
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Scalar product: " << prod << std::endl;
     std::cout << "Remaining vectors: " << Wektor2D::populacja() << std::endl;
 
     return 0;
